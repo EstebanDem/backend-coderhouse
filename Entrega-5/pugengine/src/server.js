e3e1b44a113ce2c196c7ac1750a5a561e898092e
EstebanDem <estebanjosedemarchi@gmail.com>
2022-03-12 19:47:38 -0300

    Completed challenge 5 - PUG engine template

---
+++ Entrega-5/pugengine/src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const Contenedor = require('./contenedor')
+const contenedor = new Contenedor("productos.json");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+
+// GET /productos | Lista de productos
+app.get('/productos', async (req, res) => {
+    const products = await contenedor.getAll();
+    res.render('list',{products:products
+    })
+})
+
+app.get('/', (req,res) => {
+    res.render('form', {})
+})
+
+app.post('/productos', async(req,res) => {
+    const {body} = req;
+    await contenedor.save(body);
+    
+})
+
+const PORT = 6065;
+const server = app.listen(PORT, () => {
+console.log(` >>>>> 🚀 Server started at http://localhost:${PORT}`)
+})
+
+server.on('error', (err) => console.log(err));
