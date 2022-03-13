c79d902d3a363052d3e76030fe472698bc278974
EstebanDem <estebanjosedemarchi@gmail.com>
2022-03-13 19:38:11 -0300

    Added Challenge-5 Ejs engine template

---
+++ Entrega-5/ejsengine/src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const Contenedor = require('./contenedor')
+const contenedor = new Contenedor("productos.json");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/productos', async (req, res) => {
+    const productos = await contenedor.getAll();
+    res.render('pages/list',{productos})
+})
+
+app.get('/', (req,res) => {
+    res.render('pages/form', {})
+})
+
+app.post('/productos', async(req,res) => {
+    const {body} = req;
+    await contenedor.save(body);
+    res.redirect('/');
+})
+
+const PORT = 4545;
+const server = app.listen(PORT, () => {
+console.log(` >>>>> 🚀 Server started at http://localhost:${PORT}`)
+})
+
+server.on('error', (err) => console.log(err));
