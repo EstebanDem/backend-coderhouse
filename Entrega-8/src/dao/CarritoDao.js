fff20634da0d125cb5169f270ec7a0093bf0d7ed
EstebanDem <estebanjosedemarchi@gmail.com>
2022-04-08 22:23:00 -0300

    Added save and delete method to CarritoDao

--- Entrega-8/src/dao/CarritoDao.js
+++ Entrega-8/src/dao/CarritoDao.js
@@ -4,4 +4,28 @@ export class CarritoDao {
 
     TABLE_NAME = 'carrito';
     ID_COLUMN = 'id';
+    
+    async save(object) {
+        try {
+            const newCarritoId = await knex.insert(object).from(this.TABLE_NAME);        
+            console.log(`✔️ Carrito agregado con ID: ${newCarritoId}.`);
+            return newCarritoId;
+        } catch (error) {
+            console.log(error);
+        } finally {
+            knex.destroy();
+        }
+    }
+    
+    async deleteById(id) {
+        try {
+            await knex.del().from(this.TABLE_NAME).where(this.ID_COLUMN, id);
+            console.log('🪦 Carrito borrado.');
+            return true;
+        } catch (error) {
+            console.log(error);
+        } finally {
+            knex.destroy();
+        }
+    }
 }
