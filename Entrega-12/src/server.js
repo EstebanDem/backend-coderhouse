e995c67f8d33519429630ab6748f76369f56afb3
EstebanDem <estebanjosedemarchi@gmail.com>
2022-05-04 22:31:24 -0300

    Added user login/logout based on .env file

--- Entrega-12/src/server.js
+++ Entrega-12/src/server.js
@@ -1,15 +1,42 @@
 import express from "express";
 import productRouter from './routes/product.js';
 import cartRouter from './routes/cart.js';
+import userRouter from './routes/user.js';
+import session from 'express-session';
+import {engine} from 'express-handlebars';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
-const PORT = 3026;
+const PORT = 3027;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static('public'));
+
+app.set('views', './src/views');
+app.set('view engine', 'hbs');
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/productos', productRouter);
 app.use('/api/carrito', cartRouter);
+app.use('/api/usuario', userRouter);
 
 const server = app.listen(PORT, () => {
     console.log(` >>>>> 🚀 Server started at http://localhost:${PORT}`)
