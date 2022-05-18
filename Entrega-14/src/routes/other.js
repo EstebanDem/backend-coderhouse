b4426623bec7f5e018a9504971561d5e7719673a
estebanjosedemarchi@gmail.com <estebanjosedemarchi@gmail.com>
2022-05-18 20:16:24 -0300

    Used fork to avoid server to freeze

--- Entrega-14/src/routes/other.js
+++ Entrega-14/src/routes/other.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
+import { fork } from 'child_process';
 
 router.get('/info', (_req, res) => {
     const processInfo = {
@@ -12,6 +13,19 @@ router.get('/info', (_req, res) => {
     };
     
     res.status(200).json(processInfo);
+})
+
+const randomNumbersGeneratorFork = fork('./src/functions/randomNumbersGenerator.js')
+
+router.get('/randoms', (req, res) => {
+    
+    const cant = req.query.cant || 5000;
+    
+    randomNumbersGeneratorFork.on('message', (resultado) => {
+        res.status(200).json(resultado);
+    })
+    randomNumbersGeneratorFork.send(cant);
+    
 })
 
 export default router;
