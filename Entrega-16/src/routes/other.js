26bf0cf1858812bb8f8e26770057aa5654e68379
estebanjosedemarchi@gmail.com <estebanjosedemarchi@gmail.com>
2022-06-10 18:27:28 -0300

    Copied Entrega-15 files into Entrega-16

---
+++ Entrega-16/src/routes/other.js
@@ -0,0 +1,35 @@
+import express from "express";
+const router = express.Router();
+import { fork } from 'child_process';
+import os from 'node:os';
+
+
+router.get('/info', (_req, res) => {
+    const processInfo = {
+        platform: process.platform,
+        version: process.version,
+        title: process.title,
+        execPath: process.execPath,
+        processId: process.pid,
+        rss: process.memoryUsage().rss,
+        numberOfProcessors: os.cpus().length
+    };
+    
+    res.status(200).json(processInfo);
+})
+
+const randomNumbersGeneratorFork = fork('./src/functions/randomNumbersGenerator.js')
+
+router.get('/randoms', (req, res) => {
+    
+    const cant = req.query.cant || 5000;
+    
+    randomNumbersGeneratorFork.on('message', (resultado) => {
+        res.status(200).json(resultado);
+    })
+    randomNumbersGeneratorFork.send(cant);
+    console.log('Lista generada')
+    
+})
+
+export default router;
